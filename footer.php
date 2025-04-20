ae13a22a3d27a4ce3f41ceeac97103c59c563970
parrot-xxiv <parrot-xxiv@users.noreply.github.com>
2025-04-20 15:11:52 +0800

    use secondary-footer

--- footer.php
+++ footer.php
@@ -1,6 +1,2 @@
-    <footer>
-        <p>&copy; <?php echo date( 'Y' ); ?> My Custom Theme. All rights reserved.</p>
-    </footer>
-    <?php wp_footer(); ?>
-</body>
-</html>
+
+<?php get_template_part('template-parts/secondary-footer'); ?>
