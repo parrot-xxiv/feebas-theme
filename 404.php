188017c8303543a892bff214af491d99c87caffb
parrot-xxiv <parrot-xxiv@users.noreply.github.com>
2025-05-14 15:17:53 +0800

    add 404

--- 404.php
+++ 404.php
@@ -1,1 +1,16 @@
-<div>404 Not found</div>
+<?php get_header(); ?>
+
+<main class="bg-[#151515] text-[#DADCDC] bg-[#1C1C1C]">
+    <div class="flex items-center justify-center min-h-screen">
+        <div class="text-center dark:bg-[#151515] dark:text-[#DADCDC] p-8 rounded-lg shadow-xl">
+            <h1 class="text-6xl font-bold text-[#FF7F32] animate-pulse">404</h1>
+            <p class="mt-4 text-xl">Oops! The page you're looking for doesn't exist.</p>
+            <a href="/" class="mt-6 inline-block px-6 py-3 text-[#151515] bg-[#FF7F32] rounded-lg text-lg font-semibold hover:bg-[#DADCDC] hover:text-[#151515] transition duration-300">Go Back Home</a>
+        </div>
+    </div>
+</main>
+
+<?php get_footer(); ?>
+</body>
+
+</html>
